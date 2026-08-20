dd5455d1f994482dc73ecffa53b4690789ec159d
Dipali Karkhile <deepakarkhile369@gmail.com>
2026-08-20 12:47:18 +0000

    commit

--- models/example/Staging/customer.sql
+++ models/example/Staging/customer.sql
@@ -1,6 +1,6 @@
 
     select
-    id ,
+    try cast(id as int) as id,
     first_name,
     last_name
-    from {{Sales.raw_schema.customerdata}}
+    from {{sources('datafeed_shared_schema','customerdata')}}
